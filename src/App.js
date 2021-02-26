4097176318394b21aafbb098c139619a7f35fde3
jdavidmartinez/git config --global user.name jdavidmartinez <jdavid.martinez@gmail.com>
2021-02-26 12:29:32 -0500

    first task module 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, this is a react App</h1>
+        <p>This is really Working!</p>
+        <Person />
+        <UserInput />
+        <UserOutput userName ="Pepito"/>
+        <UserOutput userName="Fulana" />
+      
       </div>
     );
   }
---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const userInput = () => {
+    return (
+        <div>
+            <p>User Input</p>
+            <input placeholder="Write your text here"></input>
+        </div>
+    )
+}
+
+export default userInput;
